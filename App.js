1eeedba10e9bf5d8010996c739f49cd963944ca2
Edgar Talledos <onimenotsuki@gmail.com>
2021-01-20 12:26:51 -0600

    Agregado firebase context

--- App.js
+++ App.js
@@ -1,17 +1,20 @@
-import React from 'react'
-import {  YellowBox } from 'react-native'
-import { Provider as PaperProvider } from 'react-native-paper';
-import { NavigationContainer } from "@react-navigation/native";
-import Navigation from "./src/navigation/Navigation";
-import "firebase/auth";
+import React from 'react';
+import {YellowBox} from 'react-native';
+import {Provider as PaperProvider} from 'react-native-paper';
+import {NavigationContainer} from '@react-navigation/native';
+import Navigation from './src/navigation/Navigation';
 
-YellowBox.ignoreWarnings(['Setting a timer for a long period of time'])
-export default function App() {  
+import {FirebaseProvider} from './src/contexts/firebase-context';
+
+YellowBox.ignoreWarnings(['Setting a timer for a long period of time']);
+export default function App() {
   return (
     <PaperProvider>
-      <NavigationContainer>
-      <Navigation/>
-      </NavigationContainer>
+      <FirebaseProvider>
+        <NavigationContainer>
+          <Navigation />
+        </NavigationContainer>
+      </FirebaseProvider>
     </PaperProvider>
-  )
+  );
 }
